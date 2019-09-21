fe11c00b8303670341c9d97c3852b06c4998d586
John Patek <jpatek@ubiqsecurity.com>
2019-09-21 16:08:09 -0700

    Added documentation to macros in Configuration.hpp

--- include/Basic/Configuration.hpp
+++ include/Basic/Configuration.hpp
@@ -2,17 +2,23 @@
 #pragma once
 
 #if defined(_WIN32)
+/** Windows */
 #define EdeaWindows
 #elif defined(__ANDROID__)
+/** Android */
 #define EdeaAndroid
 #elif defined(__linux__)
+/** Linux */
 #define EdeaLinux
 #elif defined(__APPLE__)
 #if defined(TARGET_OS_IPHONE)
+/** iOS */
 #define EdeaIos
 #elif defined(TARGET_OS_MAC)
+/** Mac OS X */
 #define EdeaMac
 #endif
 #else
+/** Undefined */
 #error "This Operating System is not supported by Edea"
 #endif
