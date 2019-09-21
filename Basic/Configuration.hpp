f1a3f5be7df40053e094ebca5b663aa21ef57342
IrisTechnologiesOfficial <40865154+IrisTechnologiesOfficial@users.noreply.github.com>
2019-09-21 21:21:49 +0200

    Add files via upload

---
+++ Basic/Configuration.hpp
@@ -0,0 +1,18 @@
+//Copyright 2019 Samuel Alonso, All Rights Reserved
+#pragma once
+
+#if defined(_WIN32)
+#define EdeaWindows
+#elif defined(__ANDROID__)
+#define EdeaAndroid
+#elif defined(__linux__)
+#define EdeaLinux
+#elif defined(__APPLE__)
+#if defined(TARGET_OS_IPHONE)
+#define EdeaIos
+#elif defined(TARGET_OS_MAC)
+#define EdeaMac
+#endif
+#else
+#error "This Operating System is not supported by Edea"
+#endif
