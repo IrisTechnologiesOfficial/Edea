7578cdf8ca5707bca3264f50eb22a09afe25ac18
IrisTechnologiesOfficial <40865154+IrisTechnologiesOfficial@users.noreply.github.com>
2019-09-21 12:48:01 +0200

    Add files via upload

---
+++ Edea.cpp
@@ -0,0 +1,12 @@
+//Copyright 2019 Samuel Alonso, All Rights Reserved
+#include <Core/MainWindow.hpp>
+
+#if defined(EdeaWindows)
+int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
+{
+    //FreeConsole();
+    AppInstance = hInstance;
+    Edea::MainWindow::Draw();
+    Edea::MainWindow::Close();
+}
+#endif
