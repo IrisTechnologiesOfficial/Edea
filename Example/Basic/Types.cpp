1642ef723ede73052ab729acbc4412654044f27c
IrisTechnologiesOfficial <40865154+IrisTechnologiesOfficial@users.noreply.github.com>
2019-09-23 01:02:54 +0200

    Add files via upload

---
+++ Example/Basic/Types.cpp
@@ -0,0 +1,15 @@
+#include <Basic/Types.hpp>
+
+void EdeaTypes()
+{
+    s8 signed8bits = 0;
+    u8 unsigned8bits = 0;
+    s16 signed16bits = 0;
+    u16 unsigned16bits = 0;
+    s32 signed32bits = 0;
+    u32 unsigned32bits = 0;
+    f32 float32bits = 0.0f;
+    s64 signed64bits = 0;
+    u64 unsigned64bits = 0;
+    f64 float64bits = 0.0;
+}
